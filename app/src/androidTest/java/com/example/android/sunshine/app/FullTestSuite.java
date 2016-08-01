e5e9aff94c5aecf9017df4fbf7fc7105de718528
Rana Ahmed <ranaahmed554@gmail.com>
2016-08-01 17:14:48 +0300

    android fundamentals

---
+++ app/src/androidTest/java/com/example/android/sunshine/app/FullTestSuite.java
@@ -0,0 +1,20 @@
+package com.example.android.sunshine.app;
+
+/**
+ * Created by Rana on 12/3/2015.
+ */
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class FullTestSuite extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class)
+                .includeAllPackagesUnderHere().build();
+    }
+
+    public FullTestSuite() {
+        super();
+    }
+}
